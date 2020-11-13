7f15426d0c3804bd5ede71a5aa98e8f5eb21149f
Othon Brück <othon.bruck@hotmail.com>
2020-11-13 10:34:25 -0300

    feat: Criando componente Lista e Cards

--- src/components/Lista/index.js
+++ src/components/Lista/index.js
@@ -1,15 +1,42 @@
+import { makeStyles } from "@material-ui/core";
 import React from "react";
 import { usePesquisaContext } from "../../context/PesquisaContext";
+import FilmesCard from "../FilmesCard/index";
+import SerieCard from "../SerieCard/index";
+import PessoaCard from "../PessoaCard/index";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  lista: {
+    display: "flex",
+    listStyleType: "none",
+    flexWrap: "wrap",
+    justifyContent: "center",
+  },
+}));
 
 export default function Index() {
-  const { pesquisa } = usePesquisaContext();
+  const { pesquisa, tipo } = usePesquisaContext();
+  const classes = useStyles();
+
+  // dado tipo === pessoa              lista imprima <PessoaCard/>
+  // dado tipo === filme              lista imprime <FilmeCard/>
+  // dado tipo === serie              lista imprime <SerieCard/>
 
   return (
     <div>
-      <ul>
-        {pesquisa.map((task, index) => (
-          <h1>{task.vote_average}</h1>
-        ))}
+      <ul className={classes.lista}>
+        {pesquisa.map((task, index) => {
+          if (tipo === "pessoa") {
+            return <PessoaCard />;
+          } else if (tipo === "filme") {
+            return <FilmesCard />;
+          } else if (tipo === "serie") {
+            return <SerieCard />;
+          }
+        })}
       </ul>
     </div>
   );
